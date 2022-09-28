f8d4de9f95b584ed00f030c9dd33cfe8c7d76ffc
Utkarsh Paliwal <105985373+upaliwal1234@users.noreply.github.com>
2022-09-28 17:22:34 +0530

    Create Select_Sort.java

---
+++ Sorting/Select_Sort.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Select_Sort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
+    }
+
+    public static void Sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+}
