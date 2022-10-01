b3498a021c2472d9f42e0f8deb52359314148e92
Utkarsh Paliwal <105985373+upaliwal1234@users.noreply.github.com>
2022-10-01 19:56:08 +0530

    Add files via upload

---
+++ Pattern/Pattern_12.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Pattern_12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sp = 2 * (n - 1);
+        int i = 1;
+        while (i <= n) {
+            int j = 1;
+            while (j <= sp) {
+                System.out.print(" ");
+                j++;
+            }
+            int k = 1;
+            while (k <= i) {
+                if (k == i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print("* ! ");
+                }
+                k++;
+            }
+            System.out.println();
+            sp -= 2;
+            i++;
+        }
+        sc.close();
+    }
+}
