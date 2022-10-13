774fd6c0dbfbcbb71dca97b7ac9b0f59202e65fc
Utkarsh Paliwal <105985373+upaliwal1234@users.noreply.github.com>
2022-10-13 11:58:20 +0530

    Create Display.java

---
+++ 2D_Arrays/Display.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class basic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr = new int[n][m];
+        // System.out.println(arr.length);
+        // System.out.println(arr[0].length);
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        Display(arr);
+    }
+
+    public static void Display(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+    }
+}
