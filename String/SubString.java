a6be0f6e50b76525885096be0ff9c1b58dc0b302
Utkarsh Paliwal <105985373+upaliwal1234@users.noreply.github.com>
2022-11-02 17:12:27 +0530

    Update and rename main.java to SubString.java

--- String/SubString.java
+++ String/SubString.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class main {
+public class SubString {
     public static void main(String[] args) {
         // Scanner sc = new Scanner(System.in);
         // String str = sc.nextLine();
