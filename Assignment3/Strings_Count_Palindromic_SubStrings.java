711d3bdc0a404e6ceca431f68326bfa13736f71d
Utkarsh Paliwal <105985373+upaliwal1234@users.noreply.github.com>
2022-11-09 16:41:38 +0530

    Create Strings_Count_Palindromic_SubStrings.java

---
+++ Assignment3/Strings_Count_Palindromic_SubStrings.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.print(Palindrome_SubString(str));
+    }
+    public static int Palindrome_SubString(String str){
+        int count = 0;
+        for(int i = 0; i<str.length(); i++){
+            for(int j = i+1; j<=str.length(); j++){
+                if(Palindrome(str.substring(i,j))){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static boolean Palindrome(String str1) {
+        int i = 0;
+        int j = str1.length() - 1;
+        while (i < j) {
+            if (str1.charAt(i) != str1.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+        
+    }
+}
