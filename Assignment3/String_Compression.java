05b7cb7c8d55a718dcbc88bd3741434a517d908a
Utkarsh Paliwal <105985373+upaliwal1234@users.noreply.github.com>
2022-11-09 16:31:59 +0530

    Create String_Compression.java

---
+++ Assignment3/String_Compression.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String st = sc.nextLine();
+        System.out.print(compression(st));
+    }
+    public static String compression(String st){
+        int count = 1;
+        String s = "";
+        for(int i = 0; i < st.length(); i++){
+            if(i==st.length()-1){
+                s+= st.charAt(i)+""+count;
+                break;
+            }
+            if(st.charAt(i)== st.charAt(i+1) ){
+                count++;
+            }
+            else {
+                s = s + st.charAt(i) +""+count;
+                count =1;
+            }
+        }
+        return s;
+    }
+}
