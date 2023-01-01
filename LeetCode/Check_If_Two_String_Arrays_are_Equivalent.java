7105bc08206fdb5a1632802daf2ed41feb96ebeb
Utkarsh Paliwal <105985373+upaliwal1234@users.noreply.github.com>
2023-01-01 18:18:00 +0530

    Create Check_If_Two_String_Arrays_are_Equivalent.java

---
+++ LeetCode/Check_If_Two_String_Arrays_are_Equivalent.java
@@ -0,0 +1,21 @@
+// 1662. Check If Two String Arrays are Equivalent
+// Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
+// A string is represented by an array if the array elements concatenated in order forms the string.
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        String s1 = "";
+        String s2 = "";
+        for(int i = 0; i<word1.length;i++){
+            s1+= word1[i];
+        }
+        for(int i = 0; i<word2.length;i++){
+            s2+= word2[i];
+        }
+        if(s1.equals(s2)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
