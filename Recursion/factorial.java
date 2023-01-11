66093fe3d916034613ee870b31d934786ca311ed
Utkarsh Paliwal <105985373+upaliwal1234@users.noreply.github.com>
2023-01-11 11:44:16 +0530

    Create factorial.java

---
+++ Recursion/factorial.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(fact(n));
+    }
+
+    public static int fact(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n * (fact(n - 1));
+
+    }
+}
