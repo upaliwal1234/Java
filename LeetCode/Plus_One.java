7138bc4f1a8191ad59f40f9c7a84225ba2764d3f
Utkarsh Paliwal <105985373+upaliwal1234@users.noreply.github.com>
2023-01-17 21:44:27 +0530

    Update Plus_One.java

--- LeetCode/Plus_One.java
+++ LeetCode/Plus_One.java
@@ -24,3 +24,25 @@ class Solution {
         return arr;
     }
 }
+
+
+// Second Approach
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for(int i = digits.length-1; i>=0 ;i--){
+            if(digits[i]<9){
+                digits[i]++;
+                break;
+            }
+            else{
+                digits[i] = 0;
+            }
+        }
+        if(digits[0] == 0){
+            int[] arr = new int[digits.length+1];
+            arr[0] = 1;
+            return arr;
+        }
+        return digits;
+    }
+}
