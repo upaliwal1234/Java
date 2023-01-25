d31164870a278d6fa1798c33345fed6e40dcb050
Utkarsh Paliwal <105985373+upaliwal1234@users.noreply.github.com>
2023-01-25 17:47:58 +0530

    Create Combination_Sum.java

---
+++ LeetCode/Combination_Sum.java
@@ -0,0 +1,43 @@
+// 39. Combination Sum
+// Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates
+// where the chosen numbers sum to target. You may return the combinations in any order.
+// The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the 
+// frequency of at least one of the chosen numbers is different.
+// The test cases are generated such that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+// Example 1:
+// Input: candidates = [2,3,6,7], target = 7
+// Output: [[2,2,3],[7]]
+// Explanation:
+// 2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+// 7 is a candidate, and 7 = 7.
+// These are the only two combinations.
+
+// Example 2:
+// Input: candidates = [2,3,5], target = 8
+// Output: [[2,2,2,2],[2,3,3],[3,5]]
+// Example 3:
+
+// Input: candidates = [2], target = 1
+// Output: []
+
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+        Combination(target, candidates, list, new ArrayList<Integer>(), 0, 0);
+        return list;
+    }
+    public static void Combination(int n, int[] arr, List<List<Integer>> list,ArrayList<Integer> l, int sum, int last) {
+        if (sum == n) {
+            list.add(new ArrayList<>(l));
+            return;
+        }
+        if (sum > n) {
+            return;
+        }
+        for (int i = last; i < arr.length; i++) {
+            l.add(arr[i]);
+            Combination(n, arr, list, l, sum + arr[i], i);
+            l.remove(l.size()-1);
+        }
+    }
+}
