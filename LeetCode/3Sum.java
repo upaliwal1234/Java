daa1a10688db6463aebcb48bdb47142ae46b546a
Utkarsh Paliwal <105985373+upaliwal1234@users.noreply.github.com>
2023-01-30 22:06:04 +0530

    Create 3Sum.java

---
+++ LeetCode/3Sum.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int j = i + 1;
+            int k = n - 1;
+            while (j < k) {
+                if (nums[i] + nums[j] + nums[k] == 0) {
+                    list.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    while (j < k && nums[j] == nums[j - 1]) {
+                        j++;
+                    }
+                }
+                else if (nums[i] + nums[j] + nums[k] < 0) {
+                    j++;
+                }
+                else {
+                    k--;
+                }
+            }
+        }
+        return list;
+    }
+}
