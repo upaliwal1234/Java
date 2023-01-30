e58125d1ebb9f7a4fb654c7676985b142b4d447b
Utkarsh Paliwal <105985373+upaliwal1234@users.noreply.github.com>
2023-01-30 22:06:46 +0530

    Update 3Sum.java

--- LeetCode/3Sum.java
+++ LeetCode/3Sum.java
@@ -1,3 +1,6 @@
+// 15. 3Sum
+// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+// Notice that the solution set must not contain duplicate triplets.
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
